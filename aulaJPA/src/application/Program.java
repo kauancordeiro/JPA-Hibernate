61c80682f78d749bf00ec005145cb14b56f90339
kauancordeiro <cvitorcordeiro@gmail.com>
2023-03-14 23:01:28 -0300

    Primeiro commit da aula de hibernate e JPA

---
+++ aulaJPA/src/application/Program.java
@@ -0,0 +1,26 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import dominio.Pessoa;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		List<Pessoa> pessoa = new ArrayList<>();
+		
+		Pessoa p1 = new Pessoa(1, "Kauan", "kauan@gmail.com");
+		Pessoa p2 = new Pessoa(2, "Carlos", "carlos@gmail.com");
+		Pessoa p3 = new Pessoa(3, "Ana", "ana@gmail.com");
+		
+		System.out.println(p1);
+		System.out.println(p2);
+		System.out.println(p3);
+		
+	
+
+	}
+
+}
